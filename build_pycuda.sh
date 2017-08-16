92c362a1113469cce6fdc9389610adc1066acb9e
Nezar <nabdennur@gmail.com>
2017-08-16 19:14:28 -0400

    Remove unused script

--- build_pycuda.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-tar -xvf pycuda-2015.1.3.tar.gz && \
-cd pycuda-2015.1.3 && \
-python configure.py && \
-sed -i "s/CUDA_ENABLE_GL = False/CUDA_ENABLE_GL = True/g" siteconf.py && \
-cp /usr/local/nvidia/lib64/libcuda.so.352.79 /opt/conda/envs/graal/lib/libcuda.so && \
-make && \
-pip install -e .
